2e74f966315cad7def4b887999d0e9a6856c3070
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2025-12-05 10:49:40 -0500

    Fix path to Rofi popup config

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ~/.cache/wal/colors.sh
 
-options=$(echo "󰐥 Shutdown\n󰜉 Reboot" | rofi -dmenu -config .config/rofi/popup.rasi -theme-str 'listview {scrollbar: false;} window {width: 20%; height: 20%;}')
+options=$(echo "󰐥 Shutdown\n󰜉 Reboot" | rofi -dmenu -config ~/.config/rofi/popup.rasi -theme-str 'listview {scrollbar: false;} window {width: 20%; height: 20%;}')
 
 case "$options" in 
     "󰐥 Shutdown") shutdown now;;
